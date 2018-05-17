4c5e648d39f8f03d4ea228d51fb1484eee2446ae
antnbr <antoine.bntes@gmail.com>
2018-05-17 10:57:07 +0200

    New buttons structure

---
+++ imports/ui/buttons/buttons.js
@@ -0,0 +1,28 @@
+import { Template } from 'meteor/templating';
+
+import './buttons.html';
+import { mode } from '/imports/ui/body.js';
+
+Template.buttons.helpers({
+  mode(currentMode) {
+    return mode.get() === currentMode;
+  }
+});
+
+Template.add.events({
+  'click #ev_add': () => {
+    mode.set("edit");
+  }
+});
+
+Template.scan.events({
+  'click #ev_scan': () => {
+    mode.set("scan");
+  }
+});
+
+Template.cancel.events({
+  'click #ev_cancel': () => {
+    mode.set("articles");
+  }
+})
