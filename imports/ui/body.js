dac929ea5c7c4216cbf3dec800310ac24a0d8e07
antnbr <antoine.bntes@gmail.com>
2018-05-17 10:56:37 +0200

    Added scanner template

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,9 +5,10 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Articles } from '/imports/api/lists.js';
 import './body.html';
 import './infobar/infobar.js';
-import './buttons/addArticle.js';
+import './buttons/buttons.js';
 import './article/article.js';
 import './editor/editor.js';
+import './scanner/scanner.js';
 // import './print/print.js';
 
 export let mode = new ReactiveVar("articles");
