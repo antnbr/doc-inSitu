1f026f937136fdc56367e13bf7e35f9a7d024990
antnbr <antoine.bntes@gmail.com>
2018-05-29 11:35:06 +0200

    Created new methods file and 'max' Object

--- imports/api/lists.js
+++ imports/api/lists.js
@@ -1,10 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import SimpleSchema from 'simpl-schema';
+import base64Regex from 'base64-regex';
 
 // --- Lists declarations --- //
 export const Articles = new Mongo.Collection('articles');
 export const Pictures = new Mongo.Collection('pictures');
+
+export const maxLengths = {
+  title: 100,
+  text: 5000,
+  authname: 30,
+  authinfo: 200
+};
 
 // --- Schemas for lists --- //
 const Schemas = {};
@@ -17,22 +25,43 @@ Schemas.Articles = new SimpleSchema({
   title: {
     type: String,
     label: "Title",
-    max: 100
+    max: maxLengths.title
   },
   text: {
     type: String,
     label: "Text",
-    max: 5000
+    max: maxLengths.text
   },
   authname: {
     type: String,
     label: "Author Name",
-    max: 30
+    max: maxLengths.authname
   },
   authinfo: {
     type: String,
     label: "Author Info",
+    max: maxLengths.authinfo
+  },
+  timestamp: {
+    type: Date,
+    label: "Timestamp",
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date();
+      }
+    }
+  }
+});
+
+Schemas.Pictures = new SimpleSchema({
+  legend: {
+    type: String,
+    label: "Legend",
     max: 200
+  },
+  data: {
+    type: String,
+    label: "Data",
   },
   timestamp: {
     type: Date,
--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
+import base64Regex from 'base64-regex';
 
 import { Articles } from './lists.js';
 
@@ -8,6 +9,15 @@ if (Meteor.isServer) {
     'insertArticle': (article) => {
       check(article, Articles.simpleSchema());
       Articles.insert(article);
+    },
+    'insertPicture': (legend, data) => {
+      check(legend, String);
+      check(data, String);
+      if (base64Regex({ exact: true }).test(data)) {
+        Pictures.insert(legend, data);
+      } else {
+        console.log("Error: data URI doesn't seem valid...");
+      }
     }
   });
 }
