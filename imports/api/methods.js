0b0cdced5aab293c064448026ea565f2142481b4
antnbr <antoine.bntes@gmail.com>
2018-05-31 15:49:47 +0200

    Removed Meteor.isServer scope causing methods to be unavailable to client (obviously...)

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,23 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
-import base64Regex from 'base64-regex';
 
 import { Articles } from './lists.js';
+import { Pictures } from './lists.js';
 
-if (Meteor.isServer) {
-  Meteor.methods({
-    'insertArticle': (article) => {
-      check(article, Articles.simpleSchema());
-      Articles.insert(article);
-    },
-    'insertPicture': (legend, data) => {
-      check(legend, String);
-      check(data, String);
-      if (base64Regex({ exact: true }).test(data)) {
-        Pictures.insert(legend, data);
-      } else {
-        console.log("Error: data URI doesn't seem valid...");
-      }
-    }
-  });
-}
+Meteor.methods({
+  'insertArticle': (article) => {
+    // check(article, Articles.simpleSchema());
+    Articles.insert(article);
+  },
+
+  'insertPicture': (picture) => {
+    // check(picture, Pictures.simpleSchema());
+    Pictures.insert(picture);
+  }
+});
