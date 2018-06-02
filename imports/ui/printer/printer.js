de289520195209726963cb9a2ee05a87bc2390bf
antnbr <antoine.bntes@gmail.com>
2018-06-02 22:38:13 +0200

    New printer page counter

--- imports/ui/printer/printer.js
+++ imports/ui/printer/printer.js
@@ -1,11 +1,20 @@
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import { Articles } from '/imports/api/lists.js';
 import './printer.html';
 
+let pageCounter = 0;
+
 Template.printer.helpers({
   pages() {
-    console.log(Articles.find({}).fetch());
     return Articles.find({});
+  }
+});
+
+Template.page.helpers({
+  pNumber() {
+    pageCounter += 1;
+    return pageCounter;
   }
 });
