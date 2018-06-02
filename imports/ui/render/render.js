142b9fb79a9917ee2f8c3fce43947a69551d2a91
antnbr <antoine.bntes@gmail.com>
2018-06-02 22:37:43 +0200

    Moved article template

--- imports/ui/render/render.js
+++ imports/ui/render/render.js
@@ -4,6 +4,7 @@ import { moment } from 'meteor/momentjs:moment';
 import { Articles } from '/imports/api/lists.js';
 import { Pictures } from '/imports/api/lists.js';
 import './render.html';
+import '../article/article.js';
 import '../buttons/buttons.js';
 
 Template.render.helpers({
@@ -13,12 +14,5 @@ Template.render.helpers({
   },
   pictures() {
     return Pictures.find({});
-  }
-});
-
-Template.article.helpers({
-  newTimestamp() {
-    // console.log(this);
-    return moment(this.timestamp).format("DD/MM/YYYY - HH:mm");
   }
 });
