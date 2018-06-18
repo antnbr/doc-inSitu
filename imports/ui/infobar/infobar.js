880aebd04f6ff8d7c611ecad1f38d1b55667e3b5
antnbr <antoine.bntes@gmail.com>
2018-06-18 10:21:38 +0200

    New infobar with reactive informations

--- imports/ui/infobar/infobar.js
+++ imports/ui/infobar/infobar.js
@@ -1,10 +1,24 @@
 import { Template } from 'meteor/templating';
-import { moment } from 'meteor/momentjs:moment';
+import { moment } from 'meteor/momentjs:moment';
 
 import './infobar.html';
+import { Articles } from '/imports/api/lists.js';
+import { Pictures } from '/imports/api/lists.js';
 
 Template.infobar.helpers({
-  time() {
-    return moment().locale('fr').format('dddd Do MMMM YYYY');
+  firstPost() {
+    let first = Articles.findOne({}, { sort: { timestamp: 1 }});
+    // console.log(first);
+    return moment(first.timestamp).format("DD/MM/YYYY");
+  },
+  lastPost() {
+    let last = Articles.findOne({}, { sort: { timestamp: -1 }});
+    // console.log(last);
+    return moment(last.timestamp).format("DD/MM/YYYY");
+  },
+  lastUpdate() {
+    let lastUp = Articles.findOne({}, { sort: { timestamp: -1 }});
+    // console.log(lastUp);
+    return moment(lastUp.timestamp).format("HH:mm")
   }
 });
