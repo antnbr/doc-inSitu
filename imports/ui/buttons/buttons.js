e168b889d4f1d666d8a82c6f33e220103866750d
antnbr <antoine.bntes@gmail.com>
2018-06-20 11:13:03 +0200

    new buttons structure

--- imports/ui/buttons/buttons.js
+++ imports/ui/buttons/buttons.js
@@ -4,33 +4,34 @@ import './buttons.html';
 import { mode } from '/imports/ui/body.js';
 // import { isCaptured } from '/imports/ui/scanner/scanner.js';
 
-Template.buttons.helpers({
-  mode(currentMode) {
-    return mode.get() === currentMode;
-  }
-});
+export var isSelected = new ReactiveVar("home");
 
 Template.mainBtns.helpers({
-  activeMode(currentMode) {
-    // simply returns true when app is not on "rendering" mode.
-    return mode.get() !== "render" && mode.get() === currentMode;
+  isSelected(currentSelect) {
+    return isSelected.get() === currentSelect;
   }
 });
 
 Template.mainBtns.events({
   'click #ev_write': (ev, inst) => {
+    isSelected.set("write");
     mode.set("edit");
+    if (window.stream.active) stream.getVideoTracks()[0].stop();
   },
   'click #ev_scan': (ev, inst) => {
+    isSelected.set("scan");
     mode.set("scan");
   },
   'click #ev_print': (ev, inst) => {
+    isSelected.set("print");
     mode.set("print");
+    if (window.stream.active) stream.getVideoTracks()[0].stop();
   }
 });
 
-Template.cancelBtn.events({
-  'click #ev_cancel': (ev, inst) => {
+Template.homeBtn.events({
+  'click #ev_home': (ev, inst) => {
+    isSelected.set("home");
     mode.set("render");
   }
-});
+})
